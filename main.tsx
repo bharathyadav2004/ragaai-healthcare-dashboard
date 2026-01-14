0a861be1ac0ba27cddd88c8a3906bc22daa43bec
Bharath Yadav <bingibharath2004@gmail.com>
2026-01-14 09:54:23 +0530

    Fix routing by adding BrowserRouter

--- main.tsx
+++ main.tsx
@@ -1,15 +1,19 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App';
-
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
+import App from "./App";
 import { store } from "./app/store";
 import "@radix-ui/themes/styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
